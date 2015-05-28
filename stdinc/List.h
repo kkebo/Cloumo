6e5d43001463d13621e3fc4d00110185c67acd04
久保健太 <kabuto669@vpn.kebo.xyz>
2015-05-28 23:01:16 +0900

    List に Iterator を実装

--- stdinc/List.h
+++ stdinc/List.h
@@ -3,6 +3,7 @@
 template <typename T>
 class List {
 protected:
+	int _length = 0;
 	class Node {
 	public:
 		T data;
@@ -10,11 +11,16 @@ protected:
 		
 		Node() = default;
 		Node(T newData, Node *p = nullptr, Node *n = nullptr) : data(newData), prev(p), next(n) {}
-	} *dummy;
+	} *dummy, *dummyTail;
 	// dummy の prev が head で，next が tail
 	
 public:
-	List() : dummy(new Node()) {}
+	const int &length = _length;
+
+	List() : dummy(new Node), dummyTail(new Node) {
+		dummy->prev = dummy->next = dummyTail;
+		dummyTail->prev = dummy;
+	}
 	
 	virtual ~List() {
 		// dummy から順に解放
@@ -39,12 +45,67 @@ public:
 	
 	void append(const T &data) {
 		Node *node = new Node(data, dummy->next);
-		if (!dummy->prev) {
+		if (dummy->prev == dummyTail) {
 			// 空だったとき head も tail も node
 			dummy->prev = dummy->next = node;
+			node->next = dummyTail;
+			dummyTail->prev = node;
 		} else {
 			// tail の次を node にして tail も node
 			dummy->next = dummy->next->next = node;
+			node->next = dummyTail;
+			dummyTail->prev = node;
 		}
+		++_length;
+	}
+	
+	struct Iterator {
+	private:
+		Node *node;
+	
+	public:
+		friend class List;
+		Iterator &operator ++() {
+			if (node->next) {
+				node = node->next;
+			}
+			return *this;
+		}
+		Iterator &operator ++(int) {
+			if (node->next) {
+				node = node->next;
+			}
+			return *this;
+		}
+		Iterator &operator --() {
+			if (node->prev) {
+				node = node->prev;
+			}
+			return *this;
+		}
+		Iterator &operator --(int) {
+			if (node->prev) {
+				node = node->prev;
+			}
+			return *this;
+		}
+		bool operator!=(const Iterator &it) {
+			return node != it.node;
+		}
+		T &operator *() {
+			return node->data;
+		}
+	};
+	
+	Iterator begin() {
+		Iterator it;
+		it.node = dummy->prev;
+		return it;
+	}
+	
+	Iterator end() {
+		Iterator it;
+		it.node = dummyTail;
+		return it;
 	}
 };
