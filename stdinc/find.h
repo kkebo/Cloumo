7a2731f84b14f6ff35be1d46ffac44e5ef317edf
久保健太 <kabuto669@vpn.kebo.xyz>
2015-06-23 01:49:25 +0900

    find 関数作成

---
+++ stdinc/find.h
@@ -0,0 +1,9 @@
+#pragma once
+
+template <class InputIterator, class T>
+InputIterator find(InputIterator first, InputIterator last, const T &val) {
+	while (first != last) {
+		if (*first == val) return first;
+		++first;
+	}
+}
