6dc50cb160daa18c0c338889965a83f854708823
久保健太 <kabuto669@OS-tsukurou.local>
2015-06-24 20:29:16 +0900

    find に戻り値がない場合がある問題を修正

--- stdinc/find.h
+++ stdinc/find.h
@@ -3,7 +3,8 @@
 template <class InputIterator, class T>
 InputIterator find(InputIterator first, InputIterator last, const T &val) {
 	while (first != last) {
-		if (*first == val) return first;
+		if (*first == val) break;
 		++first;
 	}
+	return first;
 }
